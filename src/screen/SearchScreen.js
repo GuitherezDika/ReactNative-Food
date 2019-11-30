f119fc2da7f1b90e39d07b5f02076246af77594c
GuitherezDika <guitherez.git@gmail.com>
2019-11-30 23:37:38 +0700

    Making Hooks Reuseable

--- src/screen/SearchScreen.js
+++ src/screen/SearchScreen.js
@@ -1,19 +1,33 @@
 import React,{useState}  from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import SearchBar from '../components/SearchBar';
+import useResults from '../hooks/useResults';
+import ResultList from '../components/ResultsList';
 
 const SearchScreen = () => {
    const [term, setTerm] = useState('');
+   const [searchApi, results, errorMessage] = useResults();
+
+   const filterResultsByPrice = (price) => {
+      return results.filter(result => {
+         return result.price === price;
+      });
+   };
 
    return (
       <View>
          <SearchBar 
          term={term} 
-         onTermChange={newTerm=>setTerm(newTerm)}
-         onTermSubmit={()=> console.log('term was submitted')}
+         onTermChange={setTerm} 
+         onTermSubmit={() => searchApi(term)}
          />
-         <Text>Search Screen</Text>
-         <Text>{term}</Text>
+      
+         {errorMessage ? <Text>{errorMessage}</Text> : null }
+      
+         <Text>We have found {results.length} results</Text>
+         <ResultList results={filterResultsByPrice('$')} title="Cost Effective" />
+         <ResultList results={filterResultsByPrice('$$')} title="Bit Pricier" />
+         <ResultList results={filterResultsByPrice('$$$')} title="Big Spender" />
       </View>
    )
 };
